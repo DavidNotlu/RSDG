c96dca03cce42e73410155f61d70452f5bfd221d
David Lu <david07.lu@gmail.com>
2023-04-14 22:00:15 -0500

    Create drink.py

---
+++ website/drinks.py
@@ -0,0 +1,19 @@
+drink = []
+
+class Drink:
+    def __init__(self, article, name):
+        self.article = article
+        name = name = name
+        drink.append(self)
+    def get(self):
+        return self.article, self.name
+    
+temp = ['Hot', 'Iced']
+
+hot_syrup = ['Brown Sugar', 'Cinnamon Dolce', 'Vanilla', 'Caramel', 'Hazelnut', 'Toffee Nut', 'Honey'
+             , 'White Chocolate']
+hot_coffee = ['Americano', 'Pike Place', 'Blonde Roast', 'Dark Roast', 'Caffe Misto', 'Cappuccino'
+            , 'Espresso Con Panna', 'Flat White', 'Latte', 'Macchiato', 'Mocha']
+hot_other = ['Hot Chocolate', 'White Hot Chocolate', 'Caramel Apple Spice', 'Peppermint Hot Chocolate'
+             , 'Vanilla Creme']
+hot_tea = []
